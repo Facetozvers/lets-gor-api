e27a801e30ce4aaf00c2ce3874e62b74809abdab
Facetozvers <whatthefapmc@gmail.com>
2020-12-15 21:58:03 +0700

    login and register fix

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -17,23 +17,30 @@ class UserController extends Controller
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
             $user = Auth::user();
             $success['token'] =  $user->createToken('nApp')->accessToken;
-            return response()->json(['success' => $success], $this->successStatus);
+            return response()->json(['success' => $success, 'user' => $user], $this->successStatus);
         }
         else{
-            return response()->json(['error'=>'Unauthorised'], 401);
+            return response()->json(['error'=>'Email / Password salah'], 401);
         }
     }
-
+    
     public function register(Request $request)
     {
+        $customMessages = [
+            'email.unique' => 'Email tidak tersedia',
+            'c_password.same' => 'Confirmation Password tidak cocok',
+            'noHP.unique' => 'Nomor HP tidak tersedia'
+        ];
+
         $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users',
             'password' => 'required',
-            'noHP' => 'required',
+            'noHP' => 'required|unique:users',
             'c_password' => 'required|same:password',
-        ]);
+        ], $customMessages);
 
+        
         if ($validator->fails()) {
             return response()->json(['error'=>$validator->errors()], 401);            
         }
@@ -42,9 +49,8 @@ class UserController extends Controller
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
         $success['token'] =  $user->createToken('nApp')->accessToken;
-        $success['name'] =  $user->name;
 
-        return response()->json(['success'=>$success], $this->successStatus);
+        return response()->json(['success' => $success, 'user' => $user], $this->successStatus);
     }
 
     public function details()
